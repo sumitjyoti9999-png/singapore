290c48e23c0d98aa3d6a56f0a18dd8c0df98baf9
sumitjyoti9999-png <sumitjyoti9999@gmail.com>
2026-07-25 11:32:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ function renderHTML({ title, meta, bodyContent, schema }) {
 <head>
 <meta charset="UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-//meta
+<meta name="google-site-verification" content="f_swjSKQxA8Dye1qCFyBXzBnhlnmJ2vPjFOPiLsvIvo" />
 <title>${title}</title>
 <meta name="description" content="${meta}"/>
 <meta property="og:title" content="${title}"/>
